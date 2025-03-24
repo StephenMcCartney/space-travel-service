4f15fc689bc90ca4e6c16fcbea73e6fd5cbf11a5
Stephen McCartney <stephen_mccartney1986@live.com>
2025-03-24 16:08:13 +0000

    Space travel service
    Prototype updates.

--- app/routes.js
+++ app/routes.js
@@ -9,14 +9,14 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 
-router.post('/name', function (request, response) {
+router.post('/name-moon', function (request, response) {
 
     var destination = request.session.data['destination']
     if (destination == "Moon") {
-        response.redirect("/name")
+        response.redirect("/name-moon")
     }
     else if (destination == "Mars") {
-        response.redirect("/name")
+        response.redirect("/name-mars")
     } else {
         response.redirect("no-flights")
     }
